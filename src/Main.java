d560d4525fb19081f4f43dc313ecc7d1129f2f4a
GabrieleSantosz <gabrielesdazevedo@gmail.com>
2025-05-13 03:31:36 -0300

    ajustes de carregamento de jogo

--- src/Main.java
+++ src/Main.java
@@ -12,4 +12,4 @@ public class Main {
 // Agora é possível retornar à cena desejada e continuar o jogo a partir da mesma;
 // "COMO?" adicionando a função de TelaJogo.iniciar(); no case 2 (continuar), faz entrar diretamente no loop principal do jogo.
 // E trocando o break pelo return nas cases 1 e 2 que sai do menu e não volta após retomar o jogo.
-// Na classe "TelaInicial" também deixei comentando ao lado para localizar as mudanças. =)
+// Na classe "TelaInicial" também deixei comentando ao lado para localizar as mudanças. =).
